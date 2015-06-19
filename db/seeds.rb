3a7eca89e439a969366d023a263ce1e17f169f59
tolyk <finchak007@yahoo.com>
2015-06-19 00:12:33 +0000

    add post and comment models and seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'faker'
+
+10.times do
+  Post.create!(
+    title: Faker::Lorem.sentence,
+    body: Faker::Lorem.paragraph
+    )
+end
+
+posts = Post.all
+    
+20.times do
+  Comment.create!(
+    post: posts.sample,
+    body: Faker::Lorem.paragraph
+    )
+end
+    
+p 'Nicely done Toly! Seeds have run'
+p "#{Post.count} created"
+p "#{Comment.count} created"
